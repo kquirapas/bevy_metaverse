ec7efc87e9f4e15e7f02e2b789cee5a80ff575c2
Kristian Quirapas <kquirapas@kirbyOS.local>
2023-02-21 19:19:06 +0800

    add: screen_bounding

--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,120 @@
 use bevy::prelude::*;
 
-#[derive(Component)]
-struct Person;
+const SPRITE_WIDTH: f32 = 50.0;
+const SPRITE_HEIGHT: f32 = 50.0;
+const SCREEN_WIDTH: f32 = 640.0;
+const SCREEN_HEIGHT: f32 = 480.0;
+const SPEED: f32 = 300.0;
 
 #[derive(Component)]
 struct Name(String);
 
-#[derive(Resource)]
-struct GreetTimer(Timer);
+#[derive(Component)]
+enum Direction {
+    Stay,
+    Up,
+    Down,
+    Left,
+    Right
+}
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+// #[derive(Component)]
+// enum State {
+//     Moving,
+//     Stationary
+// }
 
-    if timer.0.tick(time.delta()).just_finished() {
-        for name in query.iter() {
-            println!("Hello {}!", name.0);
+fn keyboard_input(time: Res<Time>, keyboard_input: Res<Input<KeyCode>>, mut position: Query<(&mut Direction, &mut Transform)>) {
+    for (mut direction, mut position) in &mut position {
+        // apply movement changes based on time delta
+        match *direction {
+            Direction::Up => position.translation.y += SPEED * time.delta_seconds(),
+            Direction::Down => position.translation.y -= SPEED * time.delta_seconds(),
+            Direction::Left => position.translation.x -= SPEED * time.delta_seconds(),
+            Direction::Right => position.translation.x += SPEED * time.delta_seconds(),
+            Direction::Stay => ()
+        }
+
+        // make Stay a default direction
+        *direction = Direction::Stay;
+
+        // make Transform adjustments
+        if keyboard_input.pressed(KeyCode::W) {
+            info!("Sprite moving up!");
+            *direction = Direction::Up;
+        } else if keyboard_input.pressed(KeyCode::A) {
+            info!("Sprite moving left!");
+            *direction = Direction::Left;
+        } else if keyboard_input.pressed(KeyCode::S) {
+            info!("Sprite moving down!");
+            *direction = Direction::Down;
+        } else if keyboard_input.pressed(KeyCode::D) {
+            info!("Sprite moving right!");
+            *direction = Direction::Right;
         }
     }
 }
 
-fn add_people(mut commands: Commands) {
-    commands.spawn((Person, Name("Elaina Proctor".to_string())));
-    commands.spawn((Person, Name("Renzo Hume".to_string())));
-    commands.spawn((Person, Name("Zayna Nieves".to_string())));
+/*
+ * @dev prevent the sprite from going out of bounds
+ */
+fn screen_bound(mut position: Query<(&mut Direction, &mut Transform)>) {
+    let half_screen_width = SCREEN_WIDTH / 2.0;
+    let half_screen_height = SCREEN_HEIGHT / 2.0;
+    let half_sprite_width = SPRITE_WIDTH / 2.0;
+    let half_sprite_height = SPRITE_HEIGHT / 2.0;
+
+    for (_, mut position) in &mut position {
+        if position.translation.x - half_sprite_width < -half_screen_width {
+            info!("Left Screen Collision!");
+            position.translation.x = -half_screen_width + half_sprite_width;
+        } else if position.translation.x + half_sprite_width > half_screen_width {
+            info!("Right Screen Collision!");
+            position.translation.x = half_screen_width - half_sprite_width;
+        }
+
+        if position.translation.y - half_sprite_height < -half_screen_height {
+            info!("Top Screen Collision!");
+            position.translation.y = -half_screen_height + half_sprite_height;
+        } else if position.translation.y + half_sprite_height > half_screen_height {
+            info!("Bottom Screen Collision!");
+            position.translation.y = half_screen_height - half_sprite_height;
+        }
+    }
 }
 
-// fn hello_world() {
-//     println!("hello, world!");
-// }
+fn draw(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 
-pub struct HelloPlugin;
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut App) {
-        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)))
-            .add_startup_system(add_people)
-            // .add_system(hello_world)
-            .add_system(greet_people);
-    }
+    // Player Entity
+    // Spawn entity with (SpriteBundle, Direction) tuple
+    commands.spawn((
+            SpriteBundle {
+                sprite: Sprite {
+                    color: Color::rgb(0.0, 0.0, 0.0),
+                    custom_size: Some(Vec2::new(SPRITE_WIDTH, SPRITE_HEIGHT)),
+                    ..default()
+                },
+                ..default()
+            },
+            Direction::Stay
+        )
+    );
 }
 
 fn main() {
     App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        // manage window size
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: SCREEN_WIDTH,
+                height: SCREEN_HEIGHT,
+                ..default()
+            },
+            ..default()
+        }))
+        .add_startup_system(draw)
+        .add_system(keyboard_input)
+        .add_system(screen_bound)
         .run();
 }
